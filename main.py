2bf7d6fc0bf2eeeff8da61520ab726360e96bb87
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-08-08 17:44:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -40,3 +40,20 @@ class FilterForm(Form):
     )
     search = CharField(required=False)
     filter_field = ChoiceField(choices=FILTER_CHOICES)
+
+
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('admin/', admin.site.urls),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
